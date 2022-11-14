ee0919af8eefca2058450811e812c88633716408
Jackson Jaensch <jackson.jaensch14@gmail.com>
2022-11-14 14:17:55 +1030

    Styled index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists do
+  resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
